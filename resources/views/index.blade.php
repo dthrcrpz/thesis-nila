0ba7d50816db24885dc3e814adffdf2173f267ea
Teryong <dthrcrpz@gmail.com>
2019-04-15 00:03:50 +0800

    fixed a dead link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,7 +35,7 @@
                     <div class="icon">
                         <i class="ion ion-person-add"></i>
                     </div>
-                    <a href="#" class="small-box-footer">See All <i class="fa fa-arrow-circle-right"></i></a>
+                    <a href="/users" class="small-box-footer">See All <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             @endif
