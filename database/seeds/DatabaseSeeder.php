7766c2f595fafe3df3d89891ce3831b14e7bcdc0
teryong <dthrcrpz@gmail.com>
2019-05-28 10:48:41 +0800

    okay na yung di makadownload ng file yung Guest user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
+use App\Research;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,22 @@ class DatabaseSeeder extends Seeder
             'role' => 0,
             'email' => 'admin@admin.com',
             'password' => $password,
+        ]);
+
+        $password = bcrypt('password');
+        User::create([
+            'name' => 'Guest',
+            'role' => 1,
+            'email' => 'guest@guest.com',
+            'password' => $password,
+        ]);
+
+        Research::create([
+            'title' => 'Testing',
+            'year' => '2011',
+            'abstract' => 'https://www.google.com/search?q=google&oq=google&aqs=chrome..69i57j69i60l3j69i58j69i65.4499j0j7&sourceid=chrome&ie=UTF-8',
+            'authors' => 'John Doe, Jane Du',
+            'total_downloads' => 0
         ]);
     }
 }
